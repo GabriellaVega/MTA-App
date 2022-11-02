77e078974b9a56c1433f99b7a96e58bc7f569717
Gabriella Vega <gv685@nyu.edu>
2022-11-01 22:36:46 -0400

    removed comments and added MTA source link

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MTA-App
 MTA Transit App that allows you to pin frequent stops and transit lines.
+
+These are the sources that helped me throughout this project:
+    - http://web.mta.info/developers/developer-data-terms.html
--- server/server.js
+++ server/server.js
@@ -1,15 +1,11 @@
-// require("dotenv").config()
 const express = require("express");
 const https = require("https");
-// const GtfsRealtimeBindings = require("gtfs-realtime-bindings");
-// const bodyParser = require("body-parser");
 const Redis = require("redis")
 
 const client = Redis.createClient()
 client.on('error', (err) => console.log('Redis Client Error', err));
 
 const server = express();
-// server.use(bodyParser.urlencoded({extended:true}));
 
 const url = "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace";
 
@@ -19,14 +15,6 @@ server.get("/", function(req, res) {
 })
 // server.get("getTrains/:stopID", function(req, res) {
 //   // console.log(req.params.stopID)
-//   console.log("here")
-//   client.hgetall(req.params.stop, function(err, object) {
-//     if (err) {
-//       console.log(err);
-//     }
-//     console.log(object);
-//   })
-// }); 
 
 server.listen(3000, function() {
   console.log("Server is running on port 3000.");
