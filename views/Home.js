42e89e365e0a0bdde522170abe560b57c4a62df5
Gabriella Vega <gv685@nyu.edu>
2022-11-30 15:14:51 -0500

    home page

--- views/Home.js
+++ views/Home.js
@@ -1,16 +1,51 @@
-import { Button, Text } from 'react-native';
+import { View, Pressable, Text, StyleSheet } from 'react-native';
 
 export default function Home(props) {
-  return (<>
-    <Text> Welcome to the MTA App ! </Text>
-    <Button
-      title="Favorites"
-      onPress={() => props.setCurrentView("favorites")}
-    />
-    <Button
-      title="Search"
-      onPress={() => props.setCurrentView("search")}
-    />
-    </>
+  return (
+    <View style={styles.appContainer}>
+      <Text style={styles.title}> Welcome to the MTA App ! </Text>
+      <Pressable
+        style={[styles.button, styles.favorties]}
+        title="Favorites"
+        onPress={() => props.setCurrentView("favorites")}>
+          <Text>Favorites</Text>
+      </Pressable>
+      <Pressable
+        style={[styles.button, styles.search]}
+        title="Search"
+        onPress={() => props.setCurrentView("search")}
+        >
+          <Text>Search</Text>
+      </Pressable>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  appContainer: {
+    backgroundColor: "rgba(250, 250, 100, 0.4)",
+    display: "flex",
+    height: 800
+  },
+  favorties: {
+    backgroundColor: "rgba(20, 200, 50, 0.2)",
+    color: "rgba(250, 80, 160, 0.8)"
+  },
+  search: {
+    backgroundColor: "rgba(240, 140, 250, 0.2)",
+    color: "rgba(20, 160, 255, 0.8)"
+  },
+  button: {
+    height: 50,
+    width: 200,
+    borderRadius: 50,
+    margin: 10,
+    textAlign: "center",
+    justifyContent: "center",
+    marginLeft: "40%"
+  },
+  title: {
+    fontSize: 75,
+    marginLeft: "20%",
+  }
+})
